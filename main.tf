b03a5acf34611545a8d636f1bbd6f8822995fe70
Bhargava Naidu <bhargava@bootlabstech.com>
2025-07-07 17:20:07 +0530

    new provider and cli version 5.46.0 with updated config

--- main.tf
+++ main.tf
@@ -1,5 +1,24 @@
+## Uncomment this if you want to create a s3 bucket and feed to below config
+#resource "aws_s3_bucket" "athena_results" {
+#   bucket = var.bucket
+#   force_destroy = var.force_destroy
+# }
+
 resource "aws_athena_database" "athena_db" {
   name   = var.athena_name
   bucket = var.bucket
   force_destroy = var.force_destroy
+}
+
+resource "aws_athena_workgroup" "athena_wrkgrp" {
+  name = var.athena_workgroup_name
+  state       = var.state
+  configuration {
+    result_configuration {
+      output_location = "s3://${var.bucket}/results/"
+    }
+    enforce_workgroup_configuration = true
+  }
+  depends_on = [aws_athena_database.athena_db]
+
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,33 @@
 variable "region" {
-  description = "AWS Region"
+  description = "The AWS region where resources will be created (e.g., ap-south-1, us-east-1)."
   type        = string
-}
-variable "athena_name" {
-  description = "athena_name"
-  type        = string
-}
-variable "bucket" {
-  type        = string
-}
-variable "force_destroy" {
-   type        = bool
-}
-variable "role_arn" {
-  description = " The ARN of the IAM role"
-  type = string
 }
 
+variable "athena_name" {
+  description = "The name to assign to the Amazon Athena workgroup or configuration."
+  type        = string
+}
+
+variable "bucket" {
+  description = "The name of the S3 bucket used for storing Athena query results or logs."
+  type        = string
+}
+
+variable "force_destroy" {
+  description = "Whether to allow the S3 bucket to be deleted even if it contains objects (true/false)."
+  type        = bool
+}
+
+variable "role_arn" {
+  description = "The Amazon Resource Name (ARN) of the IAM role that Athena will assume for accessing resources."
+  type        = string
+}
+variable "athena_workgroup_name" {
+  description = "Name of the Athena workgroup"
+  type        = string
+}
+variable "state" {
+  description = "Specifies the state of the Athena WorkGroup. Valid values are ENABLED or DISABLED."
+  type        = string
+  default     = "ENABLED"
+}
